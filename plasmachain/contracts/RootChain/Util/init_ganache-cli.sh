846f8a000a9c974e8bdd8823ba551e83678bfd18
mkchungs <michael@wolk.com>
2018-10-16 18:49:39 -0700

    Update plasma doc

---
+++ plasmachain/contracts/RootChain/Util/init_ganache-cli.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'Starting ganache backend'
+ganache-cli  -v -m "elephant obey fiction shift punch devote glue recipe sunset sock tube obtain" -e 1000 -i 4 -h "127.0.0.1" -p "8545"
+echo
